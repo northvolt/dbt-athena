20fa702d1e3578652dc7c096c468350d2904deb6
Gordon Ball <gordon.ball@northvolt.com>
2022-01-25 11:17:58 +0100

    Revert "Avoid clean up of table wen incremental_strategy is not insert_overwrite (#43)"
    This reverts commit 9baa80c40a833c958a7b56c1585157da965eb799.

--- dbt/include/athena/macros/adapters.sql
+++ dbt/include/athena/macros/adapters.sql
@@ -112,9 +112,7 @@
 {% endmacro %}
 
 {% macro athena__drop_relation(relation) -%}
-  {% if config.get('incremental_strategy') == 'insert_overwrite' %}
-    {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
-  {% endif %}
+  {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
   {% call statement('drop_relation', auto_begin=False) -%}
     drop {{ relation.type }} if exists {{ relation }}
   {%- endcall %}
